51c713e8ddc3ae8c1897ef7d9450308e7d1db561
PRAGYA <pragya.sharma_cs18@gla.ac.in>
2020-02-23 02:12:40 -0800

    Person.java >> getter & setter added ...01 commit

--- Contacts/src/Person.java
+++ Contacts/src/Person.java
@@ -6,5 +6,62 @@ class Person {
     private String email_Address;
     private Person next;
 
+    public Person() {
+        next = null;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getAnotherNumber() {
+        return anotherNumber;
+    }
+
+    public void setAnotherNumber(String anotherNumber) {
+        this.anotherNumber = anotherNumber;
+    }
+
+    public String getEmail_Address() {
+        return email_Address;
+    }
+
+    public void setEmail_Address(String email_Address) {
+        this.email_Address = email_Address;
+    }
+
+    public Person getNext() {
+        return next;
+    }
+
+    public void setNext(Person next) {
+        this.next = next;
+    }
+
+    /*
+    @toString Override Method
+     */
+    public String toString() {
+        return "\nFirst Name: " + firstName + "\nLast Name:" + lastName + "\nContact Number: " + contactNumber + " , " + anotherNumber + "\nEmail Address: " + email_Address;
+    }
 }
