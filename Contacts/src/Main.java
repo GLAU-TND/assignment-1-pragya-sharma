a7bef3814144bcc5d84cd3455bc021a1c7c850e7
PRAGYA <pragya.sharma_cs18@gla.ac.in>
2020-02-23 04:16:41 -0800

    Main.java>> 04commit

--- Contacts/src/Main.java
+++ Contacts/src/Main.java
@@ -9,5 +9,22 @@ public class Main {
         boolean bye = false;
         Scanner scan = new Scanner(System.in);
 
+        do {
+            System.out.println("Welcome to Rakesh's Contact List App\n" +
+                    "Press 1 to add a new contact\n" +
+                    "Press 2 to view all contacts\n" +
+                    "Press 3 to search for a contact\n" +
+                    "Press 4 to delete a contact\n" +
+                    "Press 5 to exit program ");
+            try {
+                userPressed = scan.nextInt();
+            } catch (Exception e) {
+                System.out.println("An Error occurred " + e);
+            }
+
+            switch (userPressed) {
+
+            }
+        } while (!bye);
     }
 }
