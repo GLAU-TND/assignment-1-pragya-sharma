3c7a09d4c8814fb18df318d56890553047f88f12
PRAGYA <pragya.sharma_cs18@gla.ac.in>
2020-02-23 04:14:35 -0800

    Main.java init commit

---
+++ Contacts/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ContactList newList = new ContactList();
+        int userPressed = 0;
+        String option;
+        boolean bye = false;
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
