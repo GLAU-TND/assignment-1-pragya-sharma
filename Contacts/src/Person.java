90682ac55f5785f36bb596fa1954b4ce9d6e0cd7
PRAGYA <pragya.sharma_cs18@gla.ac.in>
2020-02-23 02:03:37 -0800

    init commit

---
+++ Contacts/src/Person.java
@@ -0,0 +1,10 @@
+class Person {
+    private String firstName;
+    private String lastName;
+    private String contactNumber;
+    private String anotherNumber;
+    private String email_Address;
+    private Person next;
+
+
+}
